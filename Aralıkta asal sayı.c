66db0d0d0b1c1737e5eaaff15ec50ac21ccd1dde
Methyan <121351361+Methyan@users.noreply.github.com>
2023-01-05 00:48:09 +0300

    Add files via upload

---
+++ Aralıkta asal sayı.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(){
+	
+	int sayi1,sayi2,i,j,control=0;
+	
+	printf("Lutfen bir aralik giriniz: ");
+	scanf("%d%d",&sayi1,&sayi2);
+	
+	for (i=sayi1+1;i<sayi2;i++){
+		for (j=2;j<i-1;j++){
+			if (i%j==0){
+				control=1;
+			}
+		}
+		if (control==0){
+			printf(" %d ",i);
+		}
+		control=0;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
