0a3907b36246699772d35b9cf2276c289474e616
Methyan <121351361+Methyan@users.noreply.github.com>
2023-01-08 17:58:21 +0300

    Update Aralıkta_Asal_Sayı.c

--- Aralıkta_Asal_Sayı.c
+++ Aralıkta_Asal_Sayı.c
@@ -1,23 +1,21 @@
 #include<stdio.h>
 
 int main(){
-	
-	int sayi1,sayi2,i,j,control=0;
-	
+	int sayi1, sayi2, i, j, control = 0;
 	printf("Lutfen bir aralik giriniz: ");
-	scanf("%d%d",&sayi1,&sayi2);
-	
-	for (i=sayi1+1;i<sayi2;i++){
-		for (j=2;j<i-1;j++){
-			if (i%j==0){
-				control=1;
+	scanf("%d %d", &sayi1, &sayi2);
+
+	for (i = sayi1 + 1; i < sayi2; i++){
+		for (j = 2; j < i - 1; j++){
+			if (i % j == 0){
+				control = 1;
 			}
 		}
-		if (control==0){
-			printf(" %d ",i);
+		if (control == 0){
+			printf(" %d ", i);
 		}
-		control=0;
+		control = 0;
 	}
-	
+
 	return 0;
 }
