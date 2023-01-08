c6b7023153fc15020b714be1c38460fba7b191d3
Methyan <121351361+Methyan@users.noreply.github.com>
2023-01-08 18:00:09 +0300

    Update 5_adet_asal_bulma.c

--- 5_adet_asal_bulma.c
+++ 5_adet_asal_bulma.c
@@ -2,34 +2,29 @@
 
 int main(){
 
-
     /*  Girilen sayıdan büyük 5 tane asal
      *  sayiyi bularak ekrana yazdiriyor.
      */
 
-
-	int sayi,i,j,k=0;
-	int kontrol;
-
+	int sayi, k = 0, kontrol = 1;
 	printf("Lutfen bir sayi giriniz: ");
-	scanf("%d",&sayi);
+	scanf("%d", &sayi);
 
-	for (i=sayi+1;k<5;i++){
-		for (j=2;j<i;j++){
-			if (i%j!=0){
-				kontrol=0;
+	for (int i = sayi + 1; k < 5; i++){
+		for (int j = 2; j < i; j++){
+			if (i % j != 0){
+				kontrol = 0;
 			}
 			else{
-				kontrol=1;
+				kontrol = 1;
 				break;
 			}
 		}
-		if (kontrol==0){
-			printf(" %d ",i);
+		if (kontrol == 0){
+			printf(" %d ", i);
 			k++;
 		}
 	}
-
 	return 0;
 }
 
