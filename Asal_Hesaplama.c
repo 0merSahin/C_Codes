98ac31579d88c2f6ae8608a8a4f595112e9ce5de
Methyan <121351361+Methyan@users.noreply.github.com>
2023-01-08 17:56:57 +0300

    Update Asal_Hesaplama.c

--- Asal_Hesaplama.c
+++ Asal_Hesaplama.c
@@ -2,10 +2,10 @@
 
 int main(){
 
-    int sayi,kontrol=0;
+    int sayi, kontrol=0;
 
     printf("Lutfen asali alinacak sayiyi giriniz: ");
-    scanf("%d",&sayi);
+    scanf("%d", &sayi);
 
     if (sayi < 2) {
         printf("Girdiğiniz sayi asal değildir.\n");
@@ -13,15 +13,15 @@ int main(){
     }
 
     //i'yi 2 den başlatmamız gerekmekte. Çünkü sıfırdan veya 1 den başlatırsak her sayı 0'a ve 1'e bölneceğinden doğru cevabı bulamayız.
-    for (int i=2;i<sayi;i++){  
-        if(sayi%i==0){
-            kontrol=1;
+    for (int i = 2; i < sayi; i++){  
+        if(sayi % i == 0){
+            kontrol = 1;
             break;
         }
     }
 
-    if (kontrol==1) printf("%d sayisi asal sayi degildir.\n",sayi);
-    else printf("%d sayisi asal sayidir.\n",sayi);
+    if (kontrol == 1) printf("%d sayisi asal sayi degildir.\n", sayi);
+    else printf("%d sayisi asal sayidir.\n", sayi);
     
     return 0;
 }
