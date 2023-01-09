044999dd348570cd67429f681fd8cddde1079348
Methyan <121351361+Methyan@users.noreply.github.com>
2023-01-09 17:26:18 +0300

    Create Üçgen_Alani_Hesaplama.c

---
+++ Üçgen_Alani_Hesaplama.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    double pi = 3.141592, yaricap, alan;
+    printf("Lutfen ucgenin yaricapini giriniz: "),
+    scanf("%lf", &yaricap);
+
+    alan = pi * (pow(yaricap, 2));
+    printf("Ucgenin alani: %.4f", alan);
+
+    return 0;
+}
