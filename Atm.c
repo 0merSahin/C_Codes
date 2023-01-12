bbec710ea4934cb426412b188f8488303549f072
Methyan <121351361+Methyan@users.noreply.github.com>
2023-01-12 23:49:16 +0300

    Update Atm.c

--- Atm.c
+++ Atm.c
@@ -1,60 +1,55 @@
 #include<stdio.h>
 
 int main(){
-    int numara = 123;
-    int numara=123;
-    int bakiye=3000;
-    int gunlimit=1000;
-    int islem;
-    int tutar;
+    int numara = 123, bakiye = 3000, gunlimit = 1000, islem = 0, tutar;
 
     printf("Hosgeldiniz.\n");
     printf("Lutfen musteri numaranizi giriniz: ");
-    scanf("%d",&numara);
+    scanf("%d", &numara);
 
-    if (numara!=123){
+    if (numara != 123){
         printf("Hatali numara girdiniz!");
         return 0;
     }
 
-    if (numara==123){
+    if (numara == 123){
         printf("================================");
         printf("\n*****ISLEMLER*****\n");
         printf("[1]Para Cekme\n[2]Para Yatirma\n[3]Bakiye Sorgulama\n[4]Kart Iade\n");
-        printf("Bakiyeniz: %d",bakiye);
+        printf("Bakiyeniz: %d", bakiye);
         printf("\n================================\n");
         printf("Lutfen yapmak istediginiz islemi seciniz: ");
-        scanf("%d",&islem);
+        scanf("%d", &islem);
     }
 
     switch(islem){
         case 1:
             printf("\nLutfen cekmek istediginiz tutari giriniz: ");
-            scanf("%d",&tutar);
+            scanf("%d", &tutar);
 
-            if (tutar>bakiye){
-                printf("Bakiyenizde bu kadar para bulunmamakta!\nBakiyeniz: %d",bakiye);
+            if (tutar > bakiye){
+                printf("Bakiyenizde bu kadar para bulunmamakta!\nBakiyeniz: %d", bakiye);
                 break;
             }
-            else if (tutar>gunlimit){
-                printf("Gunluk limitten daha fazla para cekemezsiniz!\nGunluk limit: %d",gunlimit);
+            else if (tutar > gunlimit){
+                printf("Gunluk limitten daha fazla para cekemezsiniz!\nGunluk limit: %d", gunlimit);
                 break;
             }
-            else if (tutar<=gunlimit){
-                bakiye-=tutar;
-                printf("Yeni bakiyeniz: %d",bakiye);
+            else if (tutar <= gunlimit){
+                bakiye -= tutar;
+                printf("Yeni bakiyeniz: %d", bakiye);
                 break;
             }
 
         case 2:
             printf("Lutfen yatirmak istediginiz miktari giriniz: ");
-            scanf("%d",&tutar);
-            bakiye+=tutar;
-            printf("Yeni Bakiyeniz: %d",bakiye);
+            scanf("%d", &tutar);
+            bakiye += tutar;
+            printf("Yeni Bakiyeniz: %d", bakiye);
             break;
 
         case 3:
-            printf("Bakiyeniz: %d",bakiye);
+            printf("Bakiyeniz: %d", bakiye);
             break;
 
         case 4:
