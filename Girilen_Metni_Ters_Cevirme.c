827529e5309a5acb66c8952bfbd44907143848e9
Ömer Şahin <121351361+0merSahin@users.noreply.github.com>
2023-03-24 00:01:54 +0300

    Create Girilen_Metni_Ters_Cevirme.c

---
+++ Girilen_Metni_Ters_Cevirme.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    char kelime[100];
+    int sayac=-1;
+    int k=1;
+
+    printf ("Lutfen bir kelime giriniz: ");
+    gets (kelime);
+
+    for (int i = 0; k == 1; i++){
+        if (kelime[i] == '\0') k = 0;
+        sayac++;  //kelimenin uzunluðu
+    }
+
+    for (int i = sayac - 1; i >- 1; i--) printf ("%c", kelime[i]);
+
+    return 0;
+}
