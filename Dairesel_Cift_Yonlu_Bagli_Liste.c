b0ca76d04bf650f49d4b64dd4296967d7853d994
Ömer Şahin <121351361+0merSahin@users.noreply.github.com>
2023-03-23 23:55:03 +0300

    Update Dairesel_Cift_Yonlu_Bagli_Liste.c

--- Dairesel_Cift_Yonlu_Bagli_Liste.c
+++ Dairesel_Cift_Yonlu_Bagli_Liste.c
@@ -5,9 +5,9 @@ typedef struct daireselListe {
     int sayi;
     struct daireselListe *next;
     struct daireselListe *before;
-}daireselListe;
+} daireselListe;
 
-int main(){
+int main() {
 
     daireselListe *root;
     daireselListe *iter;
@@ -17,41 +17,41 @@ int main(){
     root->before = NULL;
     root->next = NULL;
     root->sayi = 10;
-    iter=root;
+    iter = root;
 
     for (int i=2;i<7;i++){
         iter->next = (daireselListe *) malloc(sizeof(daireselListe));
         iter->next->before = iter;
-        iter=iter->next;
-        son=iter;
-        iter->sayi = i*10;
+        iter = iter->next;
+        son = iter;
+        iter->sayi = i * 10;
         iter->next = NULL;
     }
 
     son->next = root;
     root->before = son;
 
-    iter=root;
-    printf("\nDuz yazdirma:\n");
-    for (int i=0 ;i<20; i++){
-        printf("%d ",iter->sayi);
-        iter=iter->next;
+    iter = root;
+    printf ("\nDuz yazdirma:\n");
+    for (int i = 0; i < 20; i++){
+        printf ("%d ", iter->sayi);
+        iter = iter->next;
     }
 
-    iter=root;
-    printf("\n\nTers yazdirma:\n");
-    for (int i=0 ;i<20; i++){
-        printf("%d ",iter->sayi);
-        iter=iter->before;
+    iter = root;
+    printf ("\n\nTers yazdirma:\n");
+    for (int i = 0; i < 20; i++){
+        printf("%d ", iter->sayi);
+        iter = iter->before;
     }
 
 
 /*  DAİRESEL LİSTE OLDUĞU İÇİN İTER HİÇBİR ZAMAN NULL KARAKTERİNE GELMİYOR ve SONSUZ DÖNÜYOR:
-    iter=root;
-    printf("\n\nSonsuz yazdirma:\n");
-    while (iter!=NULL){
-        printf("%d ",iter->sayi);
-        iter=iter->before;
+    iter = root;
+    printf ("\n\nSonsuz yazdirma:\n");
+    while (iter != NULL){
+        printf("%d ", iter->sayi);
+        iter = iter->before;
     }
 */
 
