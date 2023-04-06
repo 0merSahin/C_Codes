6583d7aecfbb940d8961c054327426c4e07c19b6
Ömer Şahin <121351361+0merSahin@users.noreply.github.com>
2023-04-07 02:05:45 +0300

    Update and rename Digit_Calculation to Digit_Calculation.c

--- Digit_Calculation.c
+++ Digit_Calculation.c
@@ -9,6 +9,6 @@ int main(){
         temp /= 10;
         digit++;
     }
-    printf("%lld sayisinin digit sayisi: %lld", num, digit);
+    printf("%lld sayisinin basamak sayisi: %lld", num, digit);
     return 0;
 }
